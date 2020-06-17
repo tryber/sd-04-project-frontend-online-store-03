f503246de972d35cbaa76dbd8fc41082a5fd8a71
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-17 18:42:05 -0300

    implemented functions

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-// import  { getCategories } from '../../services/api.js';
+import * as api from '../../services/api';
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { canRenderList: false };
+  }
+
+  async componentDidMount() {
+    const data = await api.getCategories();
+    // console.log(data)
+    const cat = await api.getProductsFromCategoryAndQuery('MLB5672');
+    console.log(cat);
+    this.setState({
+      categories: data,
+      canRenderList: true,
+    });
+  }
+
   render() {
-    // getCategories();
+    const { categories, canRenderList } = this.state;
     return (
       <nav>
         <ul>
+          {canRenderList ? categories.map((item) => <li key={item.id}>{item.name}</li>) : null}
           <li>Categoria1</li>
           <li>Categoria2</li>
           <li>Categoria3</li>
           <li>Categoria4</li>
         </ul>
       </nav>
-    )
+    );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import { fetch } from 'node-fetch';
-
 export async function getCategories() {
-  // implement here
-  // return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-  // .then(data => data.json())
-  // .then(datajson => console.log(datajson))
+  const data = fetch('https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites/MLB/categories').then((file) => file.json());
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  const data = fetch(`https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`).then((file) => file.json());
+  return data;
 }
