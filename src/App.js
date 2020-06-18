1e16099a90ec2a2cf90796d070a465bc5d3e94bb
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-17 21:59:35 -0300

    new components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
 import Main from './components/Main/Main';
+import Cart from './components/Cart/Cart';
+import Finish from './components/Finish/Finish';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+import NotFound from './components/NotFound/NotFound';
 import './App.css';
 
 // import PropTypes from 'prop-types';
@@ -15,10 +19,10 @@ function App() {
         <SideBar />
         <Switch>
           <Route exact path="/" component={Main} />
-          {/* <Route exact path="/cart" component={Cart} />
+          <Route exact path="/cart" component={Cart} />
           <Route exact path="/cart/finish" component={Finish} />
           <Route exact path="/products/:id" component={ProductDetail} />
-          <Route path="/" component={NotFound} /> */}
+          <Route path="/" component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
