8ef1994cc0ca75d8495b7eccb829528f4c7e428a
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-19 13:26:39 -0300

    more test ids

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import * as api from '../../services/api';
+
+export default class extends Component {
+  render() {
+    const { data } = this.props;
+    const { title, price, thumbnail, id } = data;
+
+    return (
+      <div className="ProductCard" data-testid="product">
+        <h3>{title}</h3>
+        <img src={thumbnail} alt={title} />
+        <p>{price}</p>
+        <Link to={`/products/${id}/${title}`}>Detalhes</Link>
+      </div>
+    );
+  }
+}
