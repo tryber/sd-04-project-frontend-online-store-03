c78116065d189e3cc805fd5c87e548e5bebb731e
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-19 23:52:00 -0300

    bora yoshi

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ class App extends Component {
       textToSearch,
       categoryId,
       cartItemsTotal,
-      setCartItems,
     } = this.state;
     return (
       <BrowserRouter>
--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -4,10 +4,10 @@ import AddBoxIcon from '@material-ui/icons/AddBox';
 export default class extends Component {
   render() {
     return (
-    <p>
-      <AddBoxIcon data-testid="product-add-to-cart" />
-      Add to cart
-    </p>
+      <p>
+        <AddBoxIcon data-testid="product-add-to-cart" />
+        Add to cart
+      </p>
     );
   }
 }
